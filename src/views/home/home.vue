e46a6c3fc9d76c8e87e9281ea2a7221624b69d33
Liao-Jun-Tao <1823152337@qq.com>
2023-04-19 04:48:54 +0800

    feizhu

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,12 +4,14 @@
     </header>
     <section >
         <suggestion />
+        
     </section>
 </template>
 
 <script setup lang="ts">
 import search from './components/homeSerach.vue'
 import suggestion from './components/Search_suggestion.vue'
+
 </script>
 
 <style scoped>
